858b0973cf8dd049806f76542c21ad9b4a49da29
hungnq18 <nguyenhungpes18@gmail.com>
2026-07-22 23:56:28 +0700

    disable devtoll

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import DisableDevtool from 'disable-devtool'
 import './index.css'
 import App from './App.jsx'
+
+if (import.meta.env.PROD) {
+  DisableDevtool({
+    disableMenu: true,
+    clearIntervalWhenDevOpenTrigger: true,
+    ondevtoolopen: (_type, closeWindow) => closeWindow(),
+  })
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
